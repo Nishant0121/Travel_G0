c92f5b7b7d916d397a18a812d4c09a191352860c
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-06-03 23:18:57 +0530

    butifull

--- frontend/src/pages/account.jsx
+++ frontend/src/pages/account.jsx
@@ -27,8 +27,26 @@ export default function Account() {
     }
   };
   return (
-    <div>
-      account <div onClick={handleLogout}>button</div>
+    <div className="flex items-center justify-center mt-10">
+      <div className="card max-w-96 dark:bg-blue-950 bg-blue-50 text-dark dark:text-light p-3 rounded-xl flex flex-col items-center justify-center">
+        <div className="image h-24 w-24 flex items-center justify-center rounded-full">
+          <img
+            className="rounded-full h-24
+            w-24"
+            src={userdata.user.profimgurl}
+            alt="profile"
+            srcSet=""
+          />
+        </div>
+        <div className="name m-3 text-center">{userdata.user.name}</div>
+        <div className="email m-3">{userdata.user.email}</div>
+        <button
+          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          onClick={handleLogout}
+        >
+          {loading ? "Logging out..." : "Logout"}
+        </button>
+      </div>
     </div>
   );
 }
